76c21f37c9cd45aa248c4e30164f7ca2af56018a
21vek <git@github.com>
2016-10-02 14:18:23 +0300

    1

--- email/three-cols-images-clients/build.php
+++ email/three-cols-images-clients/build.php
@@ -139,7 +139,7 @@ img {
       <table border="0" width="600" cellpadding="0" cellspacing="0" class="container" style="width:600px;max-width:600px">
         <tr>
           <td class="container-padding header" align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:24px;font-weight:bold;padding-bottom:12px;color:#DF4726;padding-left:24px;padding-right:24px">
-            Продаем легко уникальные готовые работы: рефераты, курсовые, дипломные проекты. Выполняем срочные контрольные, отчеты по практике и онлайн-экзамены. Закажите работу прямо сейчас, ответив на это письмо! <span style="font-family:Helvetica, Arial, sans-serif;font-size:18px;font-weight:600;color:#374550">В ответе укажите (1)тему работы, (2)тип (реферат, курсовая, диплом), (3)количество страниц, (4)уровень уникальности.</span>
+            Продаем легко уникальные готовые работы: рефераты, курсовые, дипломные проекты. Выполняем срочные контрольные, отчеты по практике и онлайн-экзамены. Закажите работу прямо сейчас, ждем вашу заявку!<br> <span style="font-family:Helvetica, Arial, sans-serif;font-size:18px;font-weight:600;color:#374550">В письме укажите (1)тему работы, (2)тип (реферат, курсовая, диплом), (3)количество страниц, (4)уровень уникальности.</span>
           <br><span style="color: green;">Новая акция: назовите желаемую стоимость работы, мы постараемся снизить наши цены!</span>
           </td>
         </tr>
